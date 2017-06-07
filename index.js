1108dc3169123b7af9ddb1689919dc1c9cee0b90
Vincent Portelli <vinport16@gmail.com>
2017-06-07 12:09:32 -0400

    made data log look nice and display all data

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ http.listen(port);
 
 var file = "db.txt";
 app.use(bodyparser.urlencoded({  //for reading forms
-  extended: true
+	extended: true
 }));
 app.use(bodyparser.json());
 
 fs.stat(file, function(err, stat) {
-    if(err == null) {
-        console.log('db.txt exists');
-    } else if(err.code == 'ENOENT') {
+	if(err == null) {
+		console.log('db.txt exists');
+	} else if(err.code == 'ENOENT') {
         // file does not exist
         fs.writeFile(file, "");
     } else {
-        console.log('Some other error: ', err.code);
+    	console.log('Some other error: ', err.code);
     }
 });
 
@@ -47,23 +47,40 @@ app.post('/data', function(req, res){
 		var html = "";
 
 		fs.readFile(file, 'utf8', function (err, filedata) {
-            if (err) throw err;
-            var content = YAML.parse(filedata);
+			if (err) throw err;
+			var content = YAML.parse(filedata);
 
-            if(content == null){
-            	content = [];
-            }
-            html += "<table>";
-            for(var i = 0; i < content.length; i++){
-            	html += "<tr> <td> <font color="+content[i].code.color+">"+content[i].code.color+"</font></td><td><font color='"+content[i].code.color+"'>"+content[i].date+"</font></td> </tr>";
-            }
-            
-            html += "</table>";
+			if(content == null){
+				content = [];
+			}
 
-            html = "<html><head><title>Data</title></head><body bgcolor=\"BBBBBB\">" + html + "</body></html>";
+			fs.readFile("data_top.html", 'utf8', function(err, filetext){
+				if (err) throw err;
+				html+=filetext;
 
-            res.send(html);
-        });
+				for(var i = content.length-1; i >= 0; i--){
+					html += "<tr style='color:"+content[i].code.color+"'> <td> "+content[i].date+" </td>"+
+							"<td> "+content[i].box1+" </td> "+
+							"<td> "+content[i].box2+" </td> "+
+							"<td> "+content[i].box3+" </td> "+
+							"<td> "+content[i].box4+" </td> "+
+							"<td> "+content[i].box5+" </td> "+
+							"<td> "+content[i].box6+" </td> "+
+							"<td> "+content[i].box7+" </td> "+
+							"<td> "+content[i].box8+" </td> "+
+							"<td> "+content[i].code.total+" </td> "+
+							"</tr>";
+				}
+
+				fs.readFile("data_bottom.html", 'utf8', function(err, filetext){
+					if (err) throw err;
+					html+=filetext;
+					res.send(html);
+				});
+
+			});
+
+		});
 
 	}else{
 		res.send("incorrect password");
@@ -74,27 +91,27 @@ io.on("connection", function(socket){
 	socket.on("saveLog", function(data){
 
 		fs.readFile(file, 'utf8', function (err, filedata) {
-            if (err) throw err;
-            var content = YAML.parse(filedata);
+			if (err) throw err;
+			var content = YAML.parse(filedata);
 
-            if(content == null){
-            	content = [];
-            }
+			if(content == null){
+				content = [];
+			}
 
-            if(data.code  == null){
-  				console.log("can't log null code data");
-            }else{
-            	content.push(data);
+			if(data.code  == null){
+				console.log("can't log null code data");
+			}else{
+				content.push(data);
 
-           		filedata = YAML.stringify(content, 2);
+				filedata = YAML.stringify(content, 2);
 
-            	fs.writeFile (file, filedata, function(err) {
-                	if (err) throw err;
-                	console.log('data written to file '+data.date);
-            	});
-            }
-            
-        });
+				fs.writeFile (file, filedata, function(err) {
+					if (err) throw err;
+					console.log('data written to file '+data.date);
+				});
+			}
+
+		});
 
 	});
 });

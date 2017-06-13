5c0c548ee09fbb3aacf724cd94b7a08fa0ab2c45
Vincent Portelli <vinport16@gmail.com>
2017-06-13 12:54:37 -0400

    switched to postgreSQL db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var sio = require('socket.io');
 var YAML = require('yamljs');
 var fs = require('fs');
+var pg = require('pg');
 var app = express();
 var http = require('http').createServer(app);
 var io = sio(http);
@@ -13,24 +14,35 @@ var pw = "password";
 //listen for port
 http.listen(port);
 
-var file = "db.txt";
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+
+const client = new pg.Client(connectionString);
+client.connect();
+
+pg.connect(connectionString, function(err, client, done) {
+  client.query('CREATE TABLE IF NOT EXISTS data(id SERIAL PRIMARY KEY, date VARCHAR(60) not null, box0 INT, box1 INT, box2 INT, box3 INT, box4 INT, box5 INT, box6 INT, box7 INT, box8 INT, total INT, color VARCHAR(10))', function(err, result) {
+    done();
+  });
+});
+
+
+app.get('/db', function (request, response) {
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('SELECT * FROM data', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send( result.rows ); }
+    });
+  });
+});
 
 app.use(bodyparser.urlencoded({  //for reading forms
 	extended: true
 }));
 app.use(bodyparser.json());
 
-
-fs.stat(file, function(err, stat) {
-	if(err == null) {
-		console.log('db.txt exists');
-	} else if(err.code == 'ENOENT') {
-        // file does not exist
-        fs.writeFile(file, "");
-    } else {
-    	console.log('Some other error: ', err.code);
-    }
-});
 
 app.get('/', function(req, res){ //when someone connects initially, send the index
 	res.sendFile(__dirname + '/index.html');
@@ -51,21 +63,14 @@ app.post('/data', function(req, res){  //construct data table HTML
 
 		var html = "";
 
-		fs.readFile(file, 'utf8', function (err, filedata) {
-			if (err) throw err;
-			var content = YAML.parse(filedata);
-
-			if(content == null){
-				content = [];
-			}
-
+		get_db(function(content){
 			fs.readFile("data_top.html", 'utf8', function(err, filetext){
 				if (err) throw err;
 				html+=filetext;
 
 				html += "<tr>";
 				for(var i = content.length-1; i >= 0; i--){
-					html += "<th class='rotate'><div style='color:"+content[i].code.color+"'><span>"+content[i].date+"</span></div></th>";
+					html += "<th class='rotate'><div style='color:"+content[i].color+"'><span>"+content[i].date+"</span></div></th>";
 				}
 				html += "</tr><tr>";
 
@@ -115,7 +120,7 @@ app.post('/data', function(req, res){  //construct data table HTML
 				html += "</tr><tr>";
 
 				for(var i = content.length-1; i >= 0; i--){
-					html += "<td>"+content[i].code.total+"</td>";
+					html += "<td>"+content[i].total+"</td>";
 				}
 				html += "</tr><tr>";
 				
@@ -130,7 +135,6 @@ app.post('/data', function(req, res){  //construct data table HTML
 				});
 
 			});
-
 		});
 
 	}else{
@@ -138,47 +142,27 @@ app.post('/data', function(req, res){  //construct data table HTML
 	}
 });
 
-io.on("connection", function(socket){ //Save data entry to file
+io.on("connection", function(socket){ //Save data entry to db
 
 	socket.on("saveLog", function(data){
 
-		fs.readFile(file, 'utf8', function (err, filedata) { //read YAML file
-			if (err) throw err;
-			var content = YAML.parse(filedata);
-
-			if(content == null){
-				content = [];
-			}
-
-			if(data.code  == null){
-				console.log("can't log null code data");
-			}else{
-				content.push(data);
-
-				filedata = YAML.stringify(content, 2);  
-
-				fs.writeFile (file, filedata, function(err) { //write data back into file
-					if (err) throw err;
-					console.log('data written to file '+data.date);
-				});
-			}
-
-		});
+		if(data.code  == null){
+			console.log("can't log null code data");
+		}else{
+			add_to_db(data);
+		}
 
 	});
 
 	socket.on("census",function(){
-		fs.readFile(file, 'utf8', function (err, filedata) { //read YAML file
-			if (err) throw err;
-			var content = YAML.parse(filedata);
-
-			if(content != null){
+		get_db(function(content){
+			if(content != []){
 				if(Date().substring(0,10) === content[content.length-1].date.substring(0,10)){
 					socket.emit("census",content[content.length-1].box0);
 				}
 			}
-
 		});
+
 	});
 
 	app.post('/download', function(req, res){  // download .csv file of data
@@ -200,10 +184,7 @@ io.on("connection", function(socket){ //Save data entry to file
 			"code, "+
 			"color,\n";
 
-			fs.readFile(file, 'utf8', function (err, filedata) { //read YAML file
-				if (err) throw err;
-				var content = YAML.parse(filedata);
-
+			get_db(function(content){
 				if(content == null){
 					content = [];
 				}
@@ -219,8 +200,8 @@ io.on("connection", function(socket){ //Save data entry to file
 					content[i].box6+", "+
 					content[i].box7+", "+
 					content[i].box8+", "+
-					content[i].code.total+", "+
-					content[i].code.color+", \n";
+					content[i].total+", "+
+					content[i].color+", \n";
 				}
 
 				fs.writeFile ("generated.csv", text, function(err) { //write data back into file
@@ -228,10 +209,45 @@ io.on("connection", function(socket){ //Save data entry to file
 					var file = __dirname + '/generated.csv';
 					res.download(file);			// here's where the download happens
 				});
-
 			});
+
+			
 		}
 		
 	});
 
 });
+
+add_to_db = function(data){
+  pg.connect(connectionString, function(err, client, done) {
+    client.query("INSERT INTO data (date, box0, box1, box2, box3, box4, box5, box6, box7, box8, total, color) VALUES ('"+data.date+"', "+
+    	data.box0+", "+
+    	data.box1+", "+
+    	data.box2+", "+
+    	data.box3+", "+
+    	data.box4+", "+
+    	data.box5+", "+
+    	data.box6+", "+
+    	data.box7+", "+
+    	data.box8+", "+
+    	data.code.total+", '"+
+    	data.code.color+"')"   , function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+    });
+  });
+}
+
+function get_db(callback){
+	pg.connect(connectionString, function(err, client, done) {
+    client.query('SELECT * FROM data', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else{
+      	callback(result.rows);
+      }
+    });
+  });
+}
